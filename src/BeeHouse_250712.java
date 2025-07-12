da31a8486e29b4b12f68968d0e1d276551cacc81
이현빈 <991357@naver.com>
2025-07-12 15:45:00 +0900

    250712
    벌집 (B2)

---
+++ src/BeeHouse_250712.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class BeeHouse_250712
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int level = 1;
+
+        int max = 1;
+
+        while (N > max)
+        {
+            max += 6 * level;
+            level++;
+        }
+
+        System.out.println(level);
+    }
+}
