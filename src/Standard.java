294b496c9d9f07a8435c0bbc8f170805e5829a1c
이현빈 <991357@naver.com>
2025-07-13 16:04:45 +0900

    250713
    덩치

--- src/Standard.java
+++ src/Standard.java
@@ -5,5 +5,11 @@ public class Standard
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
     }
 }
