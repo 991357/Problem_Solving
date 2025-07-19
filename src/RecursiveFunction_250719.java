a6210555c95eaacc70303747786088a8158943c1
이현빈 <991357@naver.com>
2025-07-19 18:40:36 +0900

    1
    1

--- src/RecursiveFunction_250719.java
+++ src/RecursiveFunction_250719.java
@@ -28,27 +28,23 @@ public class RecursiveFunction_250719
 
     public static void RecursiveFunction(int n)
     {
-        StringBuilder sb1 = new StringBuilder();
+        String underline = "____".repeat(count - n);
+
         if(n == 0)
         {
-            for (int i = 0; i < count - n; i++)
-                sb1.append("____");
 
-            sb.append(sb1 + "\"재귀함수가 뭔가요?\"").append("\n");
-            sb.append(sb1 + "\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+            sb.append(underline + "\"재귀함수가 뭔가요?\"").append("\n");
+            sb.append(underline + "\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
         }
         else
         {
-            for (int i = 0; i < count - n; i++)
-                sb1.append("____");
-
-            sb.append(sb1 + "\"재귀함수가 뭔가요?\"").append("\n");
-            sb.append(sb1 + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" + sb1 +
-                    "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" + sb1 +
+            sb.append(underline + "\"재귀함수가 뭔가요?\"").append("\n");
+            sb.append(underline + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" + underline +
+                    "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" + underline +
                     "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
             RecursiveFunction(n-1);
         }
-        sb.append(sb1 + "라고 답변하였지.").append("\n");
+        sb.append(underline + "라고 답변하였지.").append("\n");
     }
 
 }
