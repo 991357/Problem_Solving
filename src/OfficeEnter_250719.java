eda23bcde1e31d25950d39c6acb9e715cc67d8ab
이현빈 <991357@naver.com>
2025-07-19 15:34:50 +0900

    250719
    회사에있는사람(S5)

--- src/OfficeEnter_250719.java
+++ src/OfficeEnter_250719.java
@@ -1,11 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class Standard
+public class OfficeEnter_250719
 {
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Map<String, String> employeeMap = new HashMap<>();
+
+        for (int i = 0; i < N; i++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String name = st.nextToken();
+            String type = st.nextToken();
+
+            employeeMap.put(name, type);
+        }
+
+        List<String> names = new ArrayList<>();
+        for (Map.Entry<String, String> entry : employeeMap.entrySet())
+            if (entry.getValue().equals("enter"))
+                names.add(entry.getKey());
+
+        Collections.sort(names, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (String name : names)
+            sb.append(name).append("\n");
+
+        System.out.print(sb);
+
 
         // 백준 제출할 때 클래스 명 바꾸세요
         // 백준 제출할 때 클래스 명 바꾸세요
