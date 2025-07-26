e8dda9e360ad2625a6bd760d188a62717582d6d2
이현빈 <991357@naver.com>
2025-07-26 20:46:33 +0900

    250726
    파이프옮기기 1 (G5)

--- src/MovePipe_250726.java
+++ src/MovePipe_250726.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class MovePipe_250726
 {
+    static int count;
+
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,14 +16,57 @@ public class MovePipe_250726
         for (int i = 0; i < N; i++)
         {
             StringTokenizer st = new StringTokenizer(br.readLine());
+
             for (int j = 0; j < N; j++)
                 homeArr[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        // 백준 제출할 때 클래스 명 바꾸세요
-        // 백준 제출할 때 클래스 명 바꾸세요
-        // 백준 제출할 때 클래스 명 바꾸세요
-        // 백준 제출할 때 클래스 명 바꾸세요
-        // 백준 제출할 때 클래스 명 바꾸세요
+
+        // 가로 대각선 호출
+        MovePipe(homeArr, 0, 0, 1, N); // 가로 0,1 부터 시작
+
+        System.out.println(count);
+    }
+
+    static void MovePipe(int homeArr[][], int type, int x, int y, int N)
+    {
+        if(x == N - 1 && y == N - 1)
+        {
+            count++;
+            return;
+        }
+
+        // 가로
+        if(type == 0)
+        {
+            if(y + 1 < N && homeArr[x][y+1] == 0) // 가로
+                MovePipe(homeArr, 0, x,y + 1, N);
+
+            if(x + 1 < N && y + 1 < N && homeArr[x][y+1] == 0 && homeArr[x+1][y] == 0 && homeArr[x+1][y+1] == 0)  // 대각선
+                MovePipe(homeArr, 1, x+1, y+1, N);
+        }
+
+        // 대각선
+        if(type == 1)
+        {
+            if(y + 1 < N && homeArr[x][y+1] == 0) // 가로
+                MovePipe(homeArr, 0, x,y + 1, N);
+
+            if(x + 1 < N && y + 1 < N && homeArr[x][y+1] == 0 && homeArr[x+1][y] == 0 && homeArr[x+1][y+1] == 0)  // 대각선
+                MovePipe(homeArr, 1, x + 1, y + 1, N);
+
+            if(x + 1 < N && homeArr[x+1][y] == 0) // 세로
+                MovePipe(homeArr, 2, x + 1, y, N);
+        }
+
+        // 세로
+        if(type == 2)
+        {
+            if(x + 1 < N && homeArr[x+1][y] == 0) // 세로
+                MovePipe(homeArr, 2, x + 1, y, N);
+
+            if(x + 1 < N && y + 1 < N && homeArr[x][y+1] == 0 && homeArr[x+1][y] == 0 && homeArr[x+1][y+1] == 0)  // 대각선
+                MovePipe(homeArr, 1, x + 1, y + 1, N);
+        }
     }
 }
---
+++ src/NewEmployee_250726.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class NewEmployee_250726
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+    }
+}
