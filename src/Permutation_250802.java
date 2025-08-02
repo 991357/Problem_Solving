d0877bcfc2a398a547f540b87ae8e651a74894ee
이현빈 <991357@naver.com>
2025-08-02 15:26:40 +0900

    250802
    순열

---
+++ src/Permutation_250802.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+public class Permutation_250802
+{
+    static int check = 0;
+    static StringBuilder sb = new StringBuilder();
+    static boolean isStop = false;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line;
+
+        while ((line = br.readLine()) != null && !line.isEmpty())
+        {
+            StringTokenizer st = new StringTokenizer(line);
+
+            String str = st.nextToken();
+            int number = Integer.parseInt(st.nextToken());
+
+            Character arr[] = new Character[str.length()];
+
+            for(int i = 0; i < str.length(); i++)
+                arr[i] = str.charAt(i);
+
+            Character sel[] = new Character[arr.length];
+            boolean visited[] = new boolean[arr.length];
+
+            // 초기화
+            isStop = false;
+            check = 0;
+
+            // 재귀를 돌것인가 말것인가
+            int sum = 1;
+
+            for(int i = arr.length; i  > 0; i--)
+                sum *= i;
+
+            if(sum < number)
+            {
+                // 얜 못뽑음
+                sb.append(str).append(" ").append(number).append(" = ").append("No permutation").append("\n");
+            }
+            else
+            {
+                recursive(arr, sel, visited, str, 0, number);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void recursive(Character arr[], Character[] sel, boolean[] visited, String str, int idx, int checkNum)
+    {
+        if(isStop) // 이제 그만~
+            return;
+
+        // bases part
+        if(idx == arr.length)
+        {
+            check++;
+
+            if(check == checkNum) // 내가 원하던 위치
+            {
+                StringBuilder sbTemp = new StringBuilder();
+
+                for(int i = 0; i < sel.length; i++)
+                    sbTemp.append(sel[i]);
+
+                sb.append(str).append(" ").append(checkNum).append(" = ").append(sbTemp).append("\n");
+
+                // 내가 원하는거 찾았으니까 이제 그만~
+                isStop = true;
+            }
+
+            return;
+        }
+
+        // inductive part
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(isStop) // 이제 그만~
+                return;
+
+            // 방문 체크
+            if(!visited[i])
+            {
+                sel[idx] = arr[i];
+                visited[i] = true;
+                recursive(arr, sel, visited, str, idx + 1, checkNum);
+                visited[i] = false;
+            }
+        }
+    }
+}
