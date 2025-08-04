5efc5bb3cf32379ac54fcac799746ea638b201f7
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-05 00:05:50 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 144 ms, Memory: 16672 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static int count;
+    public static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        count = N;
+
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n");
+        RecursiveFunction(N);
+
+        System.out.println(sb);
+
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+        // 백준 제출할 때 클래스 명 바꾸세요
+    }
+
+    public static void RecursiveFunction(int n)
+    {
+        if(n == 0)
+        {
+            StringBuilder sb1 = new StringBuilder();
+            for (int i = 0; i < count - n; i++)
+                sb1.append("____");
+
+            sb.append(sb1 + "\"재귀함수가 뭔가요?\"").append("\n");
+            sb.append(sb1 + "\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+            sb.append(sb1 + "라고 답변하였지.").append("\n");
+        }
+        else
+        {
+            StringBuilder sb1 = new StringBuilder();
+            for (int i = 0; i < count - n; i++)
+                sb1.append("____");
+
+            sb.append(sb1 + "\"재귀함수가 뭔가요?\"").append("\n");
+            sb.append(sb1 + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" + sb1 +
+                    "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" + sb1 +
+                    "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
+            RecursiveFunction(n-1);
+            sb.append(sb1 + "라고 답변하였지.").append("\n");
+        }
+    }
+
+}
