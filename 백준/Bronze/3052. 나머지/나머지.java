1f1a747a0a09543906eb50d634c94ad9b2fbd87d
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-04 23:58:25 +0900

    [Bronze II] Title: 나머지, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        HashSet<Integer> numSet = new HashSet<>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            int n = Integer.parseInt(br.readLine());
+            numSet.add(n % 42);
+        }
+
+        System.out.println(numSet.stream().count());
+
+    }
+}
