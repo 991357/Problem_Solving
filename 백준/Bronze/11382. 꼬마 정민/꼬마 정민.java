027aac2e8207fbcbb14e948f55d20de3137fca3c
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-05 00:04:54 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken()), B = Long.parseLong(st.nextToken()), C = Long.parseLong(st.nextToken());
+        System.out.println(A + B + C);
+    }
+}
