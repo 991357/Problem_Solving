c5ff8add20952dd0340c8532bd41c7b08b03ac6a
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-05 00:05:58 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 200 ms, Memory: 15644 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    static int count;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int homeArr[][] = new int[N][N];
+
+        for (int i = 0; i < N; i++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++)
+                homeArr[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+
+        // 가로 대각선 호출
+        MovePipe(homeArr, 0, 0, 1, N); // 가로 0,1 부터 시작
+
+        System.out.println(count);
+    }
+
+    static void MovePipe(int homeArr[][], int type, int x, int y, int N)
+    {
+        if(x == N - 1 && y == N - 1)
+        {
+            count++;
+            return;
+        }
+
+        // 가로
+        if(type == 0)
+        {
+            if(y + 1 < N && homeArr[x][y+1] == 0) // 가로
+                MovePipe(homeArr, 0, x,y + 1, N);
+
+            if(x + 1 < N && y + 1 < N && homeArr[x][y+1] == 0 && homeArr[x+1][y] == 0 && homeArr[x+1][y+1] == 0)  // 대각선
+                MovePipe(homeArr, 1, x+1, y+1, N);
+        }
+
+        // 대각선
+        if(type == 1)
+        {
+            if(y + 1 < N && homeArr[x][y+1] == 0) // 가로
+                MovePipe(homeArr, 0, x,y + 1, N);
+
+            if(x + 1 < N && y + 1 < N && homeArr[x][y+1] == 0 && homeArr[x+1][y] == 0 && homeArr[x+1][y+1] == 0)  // 대각선
+                MovePipe(homeArr, 1, x + 1, y + 1, N);
+
+            if(x + 1 < N && homeArr[x+1][y] == 0) // 세로
+                MovePipe(homeArr, 2, x + 1, y, N);
+        }
+
+        // 세로
+        if(type == 2)
+        {
+            if(x + 1 < N && homeArr[x+1][y] == 0) // 세로
+                MovePipe(homeArr, 2, x + 1, y, N);
+
+            if(x + 1 < N && y + 1 < N && homeArr[x][y+1] == 0 && homeArr[x+1][y] == 0 && homeArr[x+1][y+1] == 0)  // 대각선
+                MovePipe(homeArr, 1, x + 1, y + 1, N);
+        }
+    }
+}
