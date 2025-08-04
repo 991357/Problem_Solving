ba6b6d1ce79f817f29d306e66faa06315e0870a2
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-04 23:44:55 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11736 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int computerCount = Integer.parseInt(br.readLine());
+        int nodeCount = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer>[] computerList = new ArrayList[computerCount+1];
+
+        for(int i = 0; i <= computerCount; i++)
+            computerList[i] = new ArrayList<>();
+
+        for(int i = 0; i < nodeCount; i++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            computerList[s].add(e); // 시작 - 끝
+            computerList[e].add(s); // 끝 - 시작
+        }
+
+        bfs(computerList, 1);
+
+        System.out.println(count - 1);
+    }
+
+    private static void bfs(List<Integer>[] computerList, int idx)
+    {
+        Queue<Integer> virusQ = new LinkedList<>();
+        virusQ.offer(idx);
+        boolean check[] = new boolean[computerList.length];
+        check[idx] = true;
+
+        while (!virusQ.isEmpty())
+        {
+            int cur = virusQ.poll();
+            count++;
+
+            for(int i = 0; i < computerList[cur].size(); i++)
+            {
+                if(!check[computerList[cur].get(i)])
+                {
+                    check[computerList[cur].get(i)] = true;
+                    virusQ.offer(computerList[cur].get(i));
+                }
+            }
+        }
+    }
+}
