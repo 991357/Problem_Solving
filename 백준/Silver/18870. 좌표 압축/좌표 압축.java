3b69f40a73c9e29c07f31bdf12ad339ba2d8b8bc
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-05 00:07:11 +0900

    [Silver II] Title: 좌표 압축, Time: 2196 ms, Memory: 355772 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int numArr[] = new int[N];
+        Set<Integer> tempSet = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < N; i++)
+        {
+            numArr[i] = Integer.parseInt(st.nextToken());
+            tempSet.add(numArr[i]);
+        }
+
+        List<Integer> tempList = new ArrayList<>(tempSet);
+        Collections.sort(tempList);
+
+        Map<Integer, Integer> compressMap = new HashMap<>();
+        for (int i = 0; i < tempList.size(); i++)
+            compressMap.put(tempList.get(i), i);
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : numArr)
+            sb.append(compressMap.get(num) + " ");
+        System.out.println(sb);
+    }
+}
