79a5dcef707dfea782580f2777db36afa4b300f7
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-04 23:48:36 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        List<Integer> numList = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++)
+            numList.add(str.charAt(i) - '0');
+
+        numList.sort(Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (int n : numList)
+            sb.append(n);
+        System.out.println(sb);
+    }
+}
