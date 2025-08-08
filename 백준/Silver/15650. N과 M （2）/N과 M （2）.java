bb051bc0f4135854f06e59f02e27e2a2b5b09c85
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-08 15:51:12 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] numArr = new int[N];
+        boolean[] visited = new boolean[N];
+        int[] sel = new int[M];
+
+        for(int i = 0; i < N; i++)
+            numArr[i] = i + 1;
+
+        recursive(numArr, sel, visited,0, 0, N, M, sb);
+
+        System.out.println(sb);
+    }
+
+    private static void recursive(int[] numArr, int[] sel, boolean[] visited, int idx, int k ,int N, int M, StringBuilder sb)
+    {
+        // bases part
+        if(idx == M)
+        {
+            for(int i = 0; i < M; i++)
+                sb.append(sel[i]).append(" ");
+
+            sb.append("\n");
+            return;
+        }
+
+        // inductive part
+        for(int i = k; i < N; i++)
+        {
+            if(!visited[i])
+            {
+                sel[idx] = numArr[i];
+                visited[i] = true;
+                recursive(numArr, sel, visited, idx + 1, i + 1, N, M, sb);
+                visited[i] = false;
+            }
+        }
+    }
+}
