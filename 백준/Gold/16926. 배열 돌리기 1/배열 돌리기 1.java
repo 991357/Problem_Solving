aebe054a3d80ebb76fde4de09d9e811433ff1caa
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-29 12:48:02 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 776 ms, Memory: 303220 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -29,7 +29,16 @@ public class Main {
     		RotateArr(numArr);	
 		}
     	
-    	printArr(numArr);
+    	for (int i = 0; i < numArr.length; i++) 
+    	{
+    		for (int j = 0; j < numArr[i].length; j++) 
+    			sb.append(numArr[i][j] + " ");
+    		sb.append("\n");
+		}
+    	if(sb.length() > 0)
+    		sb.setLength(sb.length() - 1);
+    	System.out.println(sb);
+    	//printArr(numArr);
     }
     
     static void RotateArr(int[][] arr)
@@ -46,16 +55,16 @@ public class Main {
 
         while (top < bottom && left < right)
         {
-            for (int j = left; j < right; j++)
+            for (int j = left; j < right; j++) // 0,0 에서부터 오른쪽으로
             	arr[top][j] = newArr[top][j + 1];
 
-            for (int i = top; i < bottom; i++)
+            for (int i = top; i < bottom; i++) // 0, M-1 부터 아래쪽으로
             	arr[i][right] = newArr[i + 1][right];
 
-            for (int j = right; j > left; j--)
+            for (int j = right; j > left; j--) // N - 1, M -1 부터 왼쪽으로
             	arr[bottom][j] = newArr[bottom][j - 1];
 
-            for (int i = bottom; i > top; i--)
+            for (int i = bottom; i > top; i--) // N-1, 0부터 위로
             	arr[i][left] = newArr[i - 1][left];
 
             top++; left++; bottom--; right--;
