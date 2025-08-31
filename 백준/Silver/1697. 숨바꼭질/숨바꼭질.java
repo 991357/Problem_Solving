451f1647f8446c9218225b248accd0f4a15735c3
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-31 17:32:12 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, K, MIN;
+    static int[] costArr;
+
+    public static void main(String[] args) throws IOException
+    {
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        MIN = Integer.MAX_VALUE;
+        costArr = new int[100001];
+
+        bfs();
+        System.out.println(MIN);
+    }
+
+    private static void bfs()
+    {
+        Deque<Integer> bfsQ = new ArrayDeque<>();
+        bfsQ.offer(N);
+        costArr[N] = 0;
+
+        while (!bfsQ.isEmpty())
+        {
+            int cur = bfsQ.poll();
+
+            if(cur == K)
+            {
+                MIN = Math.min(MIN, costArr[cur]);
+                return;
+            }
+
+            // 걷기
+            if(cur + 1 <= 100000 && costArr[cur+1] == 0)
+            {
+                bfsQ.offer(cur + 1);
+                costArr[cur+1] = costArr[cur] + 1;
+            }
+
+            if(cur - 1 >= 0 && costArr[cur-1] == 0)
+            {
+                bfsQ.offer(cur - 1);
+                costArr[cur-1] = costArr[cur] + 1;
+            }
+
+            // 순간이동
+            if(cur * 2 <= 100000 && costArr[cur * 2] == 0)
+            {
+                bfsQ.offer(cur * 2);
+                costArr[cur*2] = costArr[cur] + 1;
+            }
+        }
+    }
+}
