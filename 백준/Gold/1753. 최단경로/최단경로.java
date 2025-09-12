24cbd7547b1f84ea5fbdf2f2b6231c7ebb52abc6
이현빈 <67374247+991357@users.noreply.github.com>
2025-09-13 00:22:19 +0900

    [Gold IV] Title: 최단경로, Time: 840 ms, Memory: 120344 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int V, E;
+    static final int INF = 123456789;
+
+    static class Edge implements Comparable<Edge>
+    {
+        int e, w;
+
+        public Edge(int e, int w)
+        {
+            this.e = e;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Edge o)
+        {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+
+    static List<Edge>[] edgeArr;
+    static int startNode;
+    static int[] costArr;
+
+    public static void main(String[] args) throws IOException
+    {
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        //edgeArr = new Edge[E];
+        edgeArr = new List[V + 1];
+        for (int i = 1; i <= V; i++)
+            edgeArr[i] = new ArrayList<>();
+
+        startNode = Integer.parseInt(br.readLine());
+
+        costArr = new int[V + 1];
+        Arrays.fill(costArr, INF);
+
+        for (int i = 0; i < E; i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            edgeArr[s].add(new Edge(e, w));
+        }
+
+        dijkstra();
+
+        for (int i = 1; i < costArr.length; i++)
+            sb.append(costArr[i] == INF ? "INF" : costArr[i]).append("\n");
+
+        sb.setLength(sb.length() - 1);
+
+        System.out.println(sb);
+    }
+
+    private static void dijkstra()
+    {
+        // 다익스트라 초기화
+        PriorityQueue<int[]> dstQ = new PriorityQueue<>((e1, e2) -> {return Integer.compare(e1[1], e2[1]);});
+        costArr[startNode] = 0; // 시작점
+        dstQ.offer(new int[]{startNode, 0});
+
+        while (!dstQ.isEmpty())
+        {
+            int[] cur = dstQ.poll();
+
+            int start = cur[0];
+            int d = cur[1];
+
+            for (int i = 0; i < edgeArr[start].size(); i++)
+            {
+                // 거기까지 가는 비용 가져오기
+                int nd = d + edgeArr[start].get(i).w;
+
+                if (nd < costArr[edgeArr[start].get(i).e])
+                {
+                    costArr[edgeArr[start].get(i).e] = nd;
+                    dstQ.offer(new int[]{edgeArr[start].get(i).e, nd});
+                }
+            }
+        }
+    }
+}
