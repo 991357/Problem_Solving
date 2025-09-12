ca593c1e25f27a64bf03b61b699f80d6de797956
이현빈 <67374247+991357@users.noreply.github.com>
2025-09-12 15:13:23 +0900

    [Silver III] Title: 피보나치 함수, Time: 64 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,39 +1,42 @@
 import java.io.*;
-
-public class Main
-{
+import java.util.*;
+ 
+public class Main {    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    
+    static int T, N;
+    
     public static void main(String[] args) throws IOException
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	T = Integer.parseInt(br.readLine());
+    	
+    	for (int t = 1; t <= T; t++) 
+    	{
+    		N = Integer.parseInt(br.readLine());
+    		
+    		int[] dp = new int[N + 1];
+    		
+    		dp[0] = 0;
+    		
+    		if(N >= 1)
+    			dp[1] = 1;
+    		
 
-        int T = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int test_case = 0; test_case < T; test_case++)
-        {
-            int N = Integer.parseInt(br.readLine());
-
-            int zeroDp[] = new int[N + 1];
-            int oneDp[] = new int[N + 1];
-
-            zeroDp[0] = 1;
-            oneDp[0] = 0;
-            if(N != 0)
-            {
-                zeroDp[1] = 0;
-                oneDp[1] = 1;
-            }
-
-            for (int i = 2; i <= N; i++)
-            {
-                zeroDp[i] = zeroDp[i - 1] + zeroDp[i - 2];
-                oneDp[i] = oneDp[i - 1] + oneDp[i - 2];
-            }
-
-            sb.append(zeroDp[N]).append(" ").append(oneDp[N]).append("\n");
-        }
-
-        System.out.println(sb);
+    		for (int i = 2; i <= N; i++) 
+    			dp[i] = dp[i-2] + dp[i-1];
+    		
+    		if(N == 0)
+    			sb.append(1).append(" ").append(0).append("\n");
+    		else if(N == 1)
+    			sb.append(0).append(" ").append(1).append("\n");
+    		else
+    			sb.append(dp[N-1]).append(" ").append(dp[N]).append("\n");
+		}
+    	if(sb.length() > 0)
+    		sb.setLength(sb.length() - 1);
+    	
+    	System.out.println(sb);
     }
 }
