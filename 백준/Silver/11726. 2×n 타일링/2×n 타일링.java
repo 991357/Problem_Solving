47f64d0c2ae1f7ca2f39e949d1fdf71e5cd345fb
이현빈 <67374247+991357@users.noreply.github.com>
2025-09-12 15:36:55 +0900

    [Silver III] Title: 2×n 타일링, Time: 68 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,20 +1,28 @@
 import java.io.*;
-
-public class Main
-{
+import java.util.*;
+ 
+public class Main {    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    
+    static int N;
+    static int[] dp;
+    
     public static void main(String[] args) throws IOException
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        int dp[] = new int[N + 1];
-
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= N; i++)
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-
-        System.out.println(dp[N]);
+    	N = Integer.parseInt(br.readLine());
+    	
+    	dp = new int[N+1]; // 0 : 1*2 , 1 : 2*1
+    	
+    	dp[1] = 1;
+    	
+    	if(N>1)
+    		dp[2] = 2;
+    	
+    	for (int i = 3; i <= N; i++) 
+    		dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+    	
+    	System.out.println(dp[N]);
     }
 }
