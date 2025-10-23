11734bab88fe4f34e3b5942a97b417c4a664b8bb
이현빈 <67374247+991357@users.noreply.github.com>
2025-10-23 13:46:27 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void bfs(string begin, string target, vector<string> &words, int &answer)
+{
+    queue<pair<string, int>> bfsQ;
+    vector<bool> visited(words.size(), false);
+    bfsQ.push({begin, 0});
+
+    while (!bfsQ.empty())
+    {
+        auto cur = bfsQ.front();
+        
+        bfsQ.pop();
+
+        string beg = cur.first;
+        int cost = cur.second;
+
+        if (beg == target) 
+        {
+            answer = cost;
+            return;
+        }
+
+        for (int i = 0; i < words.size(); i++)
+        {
+            if (visited[i])
+                continue;
+
+            int cnt = 0;
+            
+            for (int j = 0; j < beg.size(); j++)
+            {
+                if (beg[j] != words[i][j])
+                    cnt++;
+            }
+                
+            if (cnt == 1)
+            {
+                visited[i] = true;
+                bfsQ.push({words[i], cost + 1});
+            }
+        }
+    }
+}
+
+int solution(string begin, string target, vector<string> words)
+{
+    int answer = 0;
+    bfs(begin, target, words, answer);
+    return answer;
+}
