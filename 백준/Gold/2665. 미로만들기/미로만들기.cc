1be3053f69887ff0fffc1dd2c4a883555b2f34c0
이현빈 <67374247+991357@users.noreply.github.com>
2025-11-23 21:08:29 +0900

    [Gold IV] Title: 미로만들기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.cc
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+// 세로가 N, 가로가 M
+
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+
+struct Node
+{
+    int x,y,cost;
+};
+
+struct Cmp 
+{
+    bool operator()(const Node& a, const Node& b) const
+    {
+        return a.cost > b.cost;
+    }
+};
+
+int dst(int N, vector<vector<int>> &mapVec)
+{
+    priority_queue<Node, vector<Node>, Cmp> dstQ;
+    dstQ.push({ 0, 0, 0 });
+    vector<vector<int>> costVec(N, vector<int>(N, 987654321));
+    costVec[0][0] = 0;
+
+    while (!dstQ.empty())
+    {
+        auto cur = dstQ.top();
+        dstQ.pop();
+
+        if (cur.cost > costVec[cur.x][cur.y])
+            continue;
+
+        if (cur.x == N - 1 && cur.y == N - 1) 
+            return cur.cost;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = cur.x + dx[i];
+            int ny = cur.y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N)
+            {
+                int nCost = cur.cost;
+
+                if (mapVec[nx][ny] == 0)
+                    nCost += 1;
+
+                if (nCost < costVec[nx][ny])
+                {
+                    costVec[nx][ny] = nCost;
+                    dstQ.push({nx, ny, nCost});
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    vector<vector<int>> mapVec(N, vector<int>(N));
+    
+    for (int i = 0; i < N; i++)
+    {
+        string str;
+        cin >> str;
+
+        for (int j = 0; j < str.length(); j++)
+            mapVec[i][j] = str[j] - '0';
+    }
+
+    int res = dst(N, mapVec);
+
+    cout << res;
+
+    return 0;
+}
