6b0b7ffb1f709d1fe5901ae5d44c96a5b87869d1
이현빈 <67374247+991357@users.noreply.github.com>
2026-01-13 13:44:22 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 68 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            sb.append(a + b).append("\n");
+        }
+    
+        System.out.println(sb);
+    }
+}
